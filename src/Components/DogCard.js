0aff02ae833ccf96ab9fc207b0d01c33f44966be
jonnyhak <jonnyhak@gmail.com>
2020-12-10 17:58:38 -0500

    refactored

--- src/Components/DogCard.js
+++ src/Components/DogCard.js
@@ -1,17 +1,39 @@
 import React from "react";
+import { render } from "react-dom";
 
-function DogCard() {
-  return (
-    <div className="card">
-      <span className="content">
-        <h2 >{/*Dog name goes here*/}</h2>
-        <img alt="" src={""} />
-      </span>
-      <span className="bark">
-        <button>Bark</button>
-      </span>
-    </div>
-  );
+class DogCard extends React.Component{
+
+  state = {
+    hasBeenClicked: false
+  }
+  
+  handleClick = () => {
+    console.log("clicked!")
+    
+    this.setState((prevState)=> ({hasBeenClicked: !prevState.hasBeenClicked}))
+  }
+
+  //common method is to use a helper for the if logic
+  
+  render() {
+    const {cardObj} = this.props
+
+    return (
+      <div className="card">
+        <span className="content">
+          <h2 >{cardObj.name}</h2>
+          <img alt="" src={cardObj.img} />
+        </span>
+        <span className="bark">
+          <button onClick={this.handleClick}>Bark</button>
+        </span>
+        
+        {this.state.hasBeenClicked ? <h2>"Bark"</h2> : null}
+      
+        
+      </div>
+    );
+  }
 }
 
 export default DogCard;
--- src/Containers/DogsList.js
+++ src/Containers/DogsList.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import {apiResponse} from '../api.js'
+import DogCard from '../Components/DogCard'
 
 class DogsList extends Component {
+  dogsArr = () => {
+    return apiResponse.map(dogEl => <DogCard cardObj={dogEl} />)
+  }
+
+
   render() {
-    return <div className="list">{/*list of dogs go here*/}</div>;
+    // return <div className="list">{/*list of dogs go here*/}</div>;
+    return <div className="list">{this.dogsArr()}</div>;
   }
 }
 
